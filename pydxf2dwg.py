3191ff0e7495f7bbe35262ba2be9ec40aa13a484
Fraiddd <frederick.coulon@gmail.com>
2023-03-17 10:42:24 +0100

    Comments

--- pydxf2dwg.py
+++ pydxf2dwg.py
@@ -1,5 +1,21 @@
 # coding: utf-8
+# Python 3.11.1
+'''
+    pydxf2dwg 1.0
 
+    convert dxf to dwg.
+
+    No install
+
+    Requirements: Autocad
+    External Modules: pyautocad
+
+    Only for Windows
+    Tested on Windows 10 and Autocad 2015
+
+    :No copyright: (!) 2023 by Frédéric Coulon.
+    :No license: Do with it what you want.
+'''
 from pyautocad import Autocad
 from tkinter import Tk, filedialog, messagebox as mb
 import time
@@ -32,31 +48,25 @@ def pydxf2dwg(acad):
                                         filetypes = [('DXF files', '*.dxf *.DXF')])
     # If files were found.
     if file_path:
+        # Hide Autocad (faster)
         hwin = gethandlewin('Autodesk AutoCAD')
         win32gui.ShowWindow(hwin, win32con.SW_HIDE)
-
+        # Loop over files
         for fil in file_path:
             # Extract file path name.
             nf = '/'.join(fil.split('/')[-1:])
+            # Figure of speech
             acdc = '(vla-get-documents (vlax-get-acad-object))'
-            # time.sleep(1)
-
             # Open a dxf
             doc.SendCommand('(vla-open ' + acdc + ' "' + fil + '")\n')
             time.sleep(2)
             # Activate 
-            doc.SendCommand('(vla-activate (vla-item ' + acdc + ' "' + nf + '"))\n')
+            doc.SendCommand('(vla-activate (vla-item ' +
+                            acdc + ' "' + nf + '"))\n')
             time.sleep(1)
             # Reconnect to Autocad in the new draw.
             acad = Autocad()            
             doc = acad.ActiveDocument
-            # Current layer "0"
-            # doc.SetVariable('clayer', "0")
-            # Insertion unit in meter.
-            # doc.SetVariable('insunits', 6)
-            # Focus
-            # acad.app.ZoomExtents()
-            # time.sleep(2)
             # Save with .dxf name.
             doc.SaveAs(fil[:-3] + 'dwg')
             time.sleep(1)
@@ -65,6 +75,8 @@ def pydxf2dwg(acad):
             # Reconnect to Autocad in the drawing1.
             acad = Autocad()            
             doc = acad.ActiveDocument
+        win32gui.ShowWindow(hwin, win32con.SW_SHOW)
+        win32gui.SetForegroundWindow(hwin)
         mb.showinfo(title='Done',
                     message= str(len(file_path)) + ' files processed.')
     else:
